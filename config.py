180e2017128907f4ce1640e2d9c215fc3b53bcb6
Btmanjusms <90103484+Btmanjusms@users.noreply.github.com>
2023-01-03 09:47:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
   BOT_TOKEN = os.getenv("BOT_TOKEN", "5775731292:AAGi-Kpz54KDeMqtqFvaR4QIiOeGNRpkI5Y")
   STRINGSESSION = os.environ.get("STRINGSESSION")
   OWNER_ID = int(os.environ.get("OWNER_ID"))
-  DATABASE_CHANNELS = os.environ.get("DATABASE_CHANNELS", ["-1001769986368"])
+  DATABASE_CHANNELS = os.environ.get("DATABASE_CHANNELS", ["-1001831635652"])
   DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://vxagnqmn:gKw_gOqVE39Ys7cPQiPvXygnhg4QTUwS@berry.db.elephantsql.com/vxagnqmn")
   DELETE_DELAY = int(os.environ.get("DELETE_DELAY", 120))
   SUBSCRIPTION_TIME = int(os.getenv("SUBSCRIPTION_TIME", "31"))
