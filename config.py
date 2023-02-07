3f718109db2de8c9711a1bca70a781b007440b72
Btmanjusms <90103484+Btmanjusms@users.noreply.github.com>
2023-02-07 21:35:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
   API_ID = int(os.environ.get("API_ID", "8321183"))
   API_HASH = os.environ.get("API_HASH", "d9102799310e7038de04d9af2679ed68")
   BOT_TOKEN = os.getenv("BOT_TOKEN", "5928657017:AAHDknS9PQPkxJ_Ua7ks7mLTQgx6U3Uhocg")
-  STRINGSESSION = os.environ.get("STRINGSESSION", "1AZWarzgBu4ZJQRIYCkKTKSPLdy3x2YgoVit8hf6zeodZMnkr_z4bvjq-0p4wAW-u3irJaucR_2aT402zWzSpUMVtUHNKcfws_FemmYSxygCL60bLmaZt4t-JkLcZKWJg6gNmHGB0oL9lIsWjQ9EjdU7Ml3_cJVWFbfekyowmBeCDfmX2zPNee2CMPOrnBSVV6KQ6Q8VS-LtR31m15D1eu7AuipHCq7qEf8nnDXCsUj4x2woqSEn1-HUaW_yb5kcCORKvMt47y9k-YvjRT9VEadtFS_UrblmyLFgsolNrPqCjGA8sn6yEyMdizx19ukO16MvQlLuU-SSUsoFvDeu5zvFOtHK3R7A=")
+  STRINGSESSION = os.environ.get("STRINGSESSION", "1BVtsOLwBuyARpm69kZG2xCZCr2MDJHewD1G1V62hXN8i-MyVTShm_upu5Q_LMZi8f8cHWYLtPWhHR2e6bkP80RyBkIsYHMjqaUOegvLqxjSHu36NGlFubmcLNzxURkOQnih0aNXLJhXNCdBqSOLpoKtfUoTWIdCKJpzpZ_VMdLr9RD5izBFu0D8YUSddVKeD2_Z1HGr_BRj-PntgpDWKZeU62-iMjVqZTIP0IoVHucSmzbx6jZFgqQu05yZi201bw6Cinoszynd1Ez8c7p6IrKOSoM5-27nUzi6XhtFDcpfxxIq_Nxc-fEw-LqLJtB3C7L9Vdi1mQTAcEEs5W-T3LPj97OXwTF4=")
   OWNER_ID = int(os.environ.get("OWNER_ID", "399726799"))
   DATABASE_CHANNELS = os.environ.get("DATABASE_CHANNELS", ["-1001896983424"])
   DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://vxagnqmn:gKw_gOqVE39Ys7cPQiPvXygnhg4QTUwS@berry.db.elephantsql.com/vxagnqmn")
